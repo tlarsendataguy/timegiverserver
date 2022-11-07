fe4979783a5a3721d9f84525f81687dee5e79018
Tom Larsen <larsenthomasj@gmail.com>
2022-11-07 05:52:06 -0500

    start adding webserver code

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module timegiverserver
 
 go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/tls"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/acme/autocert"
+	"net/http"
+	"time"
+)
+
+func main() {
+	e := mux.NewRouter()
+	e.HandleFunc(`/calculate`, nil).Methods(`POST`)
+	m := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		Cache:      autocert.DirCache(`certs`),
+		HostPolicy: autocert.HostWhitelist(`www.timegiver.app`),
+	}
+	serveTls := &http.Server{
+		Addr:         `:443`,
+		Handler:      e,
+		TLSConfig:    &tls.Config{GetCertificate: m.GetCertificate},
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	serve := &http.Server{
+		Addr:         `:80`,
+		Handler:      m.HTTPHandler(nil),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	go func() {
+		err := serve.ListenAndServe()
+		println(err.Error())
+	}()
+	err := serveTls.ListenAndServeTLS(``, ``)
+	println(err.Error())
+}
